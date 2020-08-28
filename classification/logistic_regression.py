1fd6c8e722092293d63b5a47da5ecae8217f9cd5
AmirAli <amirali.kaboli@divar.ir>
2020-08-29 00:18:15 +0430

    add weighted loss in logistic regression

--- classification/logistic_regression.py
+++ classification/logistic_regression.py
@@ -1,8 +1,10 @@
+import math
+
 import numpy as np
 import pandas as pd
 
-num_iterations = 1000
-learning_rate = 0.01
+num_iterations = 100000
+learning_rate = 1
 
 
 def sigmoid(val):
@@ -15,6 +17,7 @@ if __name__ == '__main__':
 
     labels = list(data.keys())
     X = np.array(
+        [[1 for _ in range(len(data[labels[0]]))]] +
         [[feature for feature in data[label]] for label in labels[1:-2]]
     ).T
     y = np.array(data[labels[-1]])
@@ -23,20 +26,26 @@ if __name__ == '__main__':
     classes = np.unique(y)
 
     w = np.random.randn(num_features)
-    for _ in range(num_iterations):
+    for ind in range(1, num_iterations):
         gradient_vector = np.zeros(num_features)
 
         for data_point, target in zip(X, y):
-            gradient_vector += np.dot(
-                sigmoid(
-                    np.dot(
-                        w.T,
-                        data_point
-                    )
-                ) - target,
+            prediction = sigmoid(
+                np.dot(
+                    w.T,
+                    data_point
+                )
+            )
+
+            loss_weight = 1
+            if target == 1:
+                loss_weight = 578  # class population ratio
+
+            gradient_vector += loss_weight * np.dot(
+                prediction - target,
                 data_point
             )
 
-        w = w + learning_rate / num_data_points * gradient_vector
+        w = w - learning_rate / math.sqrt(ind) * gradient_vector
 
     print(w)
